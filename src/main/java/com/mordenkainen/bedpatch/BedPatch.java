c9ee94fef8bb996a4ade1fc8fd403405ab13dd60
Mordenkainen <shardrill_t@yahoo.com>
2017-12-01 15:51:17 -0500

    Initial commit of 1.11.2 version

--- src/main/java/com/mordenkainen/bedpatch/BedPatch.java
+++ src/main/java/com/mordenkainen/bedpatch/BedPatch.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
-@MCVersion("1.10.2")
+@MCVersion("1.11.2")
 @TransformerExclusions({"com.mordenkainen.bedpatch.asmhelper"})
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 public class BedPatch implements IFMLLoadingPlugin {    
