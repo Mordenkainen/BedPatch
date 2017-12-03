2373c182ed2f7dbd55460bdb9c7977585c64e5e3
Mordenkainen <shardrill_t@yahoo.com>
2017-12-03 00:34:11 -0500

    Fixed a bug running outside the dev environment, added signing of the jar, and made the patch server side only.

--- src/main/java/com/mordenkainen/bedpatch/BedPatch.java
+++ src/main/java/com/mordenkainen/bedpatch/BedPatch.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mordenkainen.bedpatch.asmhelper.ObfHelper;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
@@ -12,8 +14,9 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @MCVersion("1.10.2")
+@IFMLLoadingPlugin.SortingIndex(1001)
 @TransformerExclusions({"com.mordenkainen.bedpatch.asmhelper"})
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptableRemoteVersions = "*", certificateFingerprint = "6bf7527e690fb5e8719b9832bce5000a3e87dfe6")
 public class BedPatch implements IFMLLoadingPlugin {    
     public static Logger logger = LogManager.getLogger("BedPatch");
     
@@ -36,7 +39,10 @@ public class BedPatch implements IFMLLoadingPlugin {
     }
 
     @Override
-    public void injectData(Map<String, Object> data) {}
+    public void injectData(Map<String, Object> data) {
+        ObfHelper.setObfuscated((Boolean) data.get("runtimeDeobfuscationEnabled"));
+        ObfHelper.setRunsAfterDeobfRemapper(true);
+    }
 
     @Override
     public String getAccessTransformerClass() {
