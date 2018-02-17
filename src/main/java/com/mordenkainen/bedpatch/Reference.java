99e19823ea9997ab4f455c6285f4d011d2de39a0
Mordenkainen <shardrill_t@yahoo.com>
2018-02-16 22:34:09 -0500

    Updated to resolve conflict with AppleCore

--- src/main/java/com/mordenkainen/bedpatch/Reference.java
+++ src/main/java/com/mordenkainen/bedpatch/Reference.java
@@ -3,7 +3,7 @@ package com.mordenkainen.bedpatch;
 public final class Reference {
 
     public static final String MOD_ID = "bedpatch";
-    public static final String MOD_VERSION = "2.0";
+    public static final String MOD_VERSION = "2.1";
     public static final String MOD_NAME = "BedPatch";
 
     private Reference() {}
