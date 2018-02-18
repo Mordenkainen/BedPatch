2241e693edfa650d6e5c92f1998a22df8709919f
Colin Godsey <crgodsey@gmail.com>
2018-02-18 16:52:19 -0700

    tab v space

--- src/main/java/com/mordenkainen/bedpatch/BedPatchFunc.java
+++ src/main/java/com/mordenkainen/bedpatch/BedPatchFunc.java
@@ -9,7 +9,7 @@ import net.minecraft.util.ClassInheritanceMultiMap;
 import net.minecraft.world.World;
 
 public class BedPatchFunc {
-	public static void onChunkUnload(World world, ClassInheritanceMultiMap<Entity>[] entityLists) {
+    public static void onChunkUnload(World world, ClassInheritanceMultiMap<Entity>[] entityLists) {
         List<EntityPlayer> players = new ArrayList<EntityPlayer>();
         for (ClassInheritanceMultiMap<Entity> classinheritancemultimap : entityLists) {
             for(EntityPlayer player : classinheritancemultimap.getByClass(EntityPlayer.class)) {
